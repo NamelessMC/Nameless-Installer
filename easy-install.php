4ab552e564fbab719e29990bf57ad0bc39c78f04
PadowYT2 <me@padow.ru>
2021-11-06 08:19:26 +0300

    Update easy-install.php

--- easy-install.php
+++ easy-install.php
@@ -2,7 +2,7 @@
 /*
  *  Made by Aberdeener
  *  https://github.com/NamelessMC/Nameless-Installer/
- *  Nameless-Installer version 1.0.3
+ *  Nameless-Installer v2 pre-release 13
  * 
  *  NamelessMC by Samerton
  *  https://github.com/NamelessMC/Nameless/
@@ -35,7 +35,7 @@ $step = $_GET['step'] ?? 'welcome';
 $zip_url = '';
 $zip_file = 'namelessmc-' . $version . '.zip';
 // These will need to be updated with each NMC release
-$zip_subdir = $version == 'v1' ? 'Nameless-1.0.21' : 'Nameless-2.0.0-pr11';
+$zip_subdir = $version == 'v1' ? 'Nameless-1.0.21' : 'Nameless-2.0.0-pr13';
 
 // Recursively copy a directory to another location. Used after extraction of the zip file
 function moveDirectory($source, $dest) {    
@@ -174,7 +174,7 @@ function minorWarning() { ?>
                     case 'select': { ?>
 
                             <p><i>Now you must choose which version of NamelessMC you want to install.</i></p>
-                            <p>NamelessMC has two versions: <b>v1 (1.0.21)</b> and <b>v2 (pr11)</b>.</p>
+                            <p>NamelessMC has two versions: <b>v1 (1.0.21)</b> and <b>v2 (pr13)</b>.</p>
                             <p><b>v2</b> is recommended by NamelessMC developers as it is a complete rewrite and provides many more functionalities - such as modules, widgets and beautiful templates.</p>
                             <br />
                             <div class="row">
@@ -191,7 +191,7 @@ function minorWarning() { ?>
                                         <h5 class="card-title" style="color: white">Recommended</h5>
                                         <img src="https://namelessmc.com/custom/templates/Nameless-Semantic/img/v2-homepage.jpg" class="card-img" alt="NamelessMC v2.0.0-pr10">
                                         <hr style="background-color: white">
-                                        <a href="?step=verify&ver=v2" class="btn btn-outline btn-version">v2.0.0-pr11</a>
+                                        <a href="?step=verify&ver=v2" class="btn btn-outline btn-version">v2.0.0-pr13</a>
                                     </div>
                                 </div>
                             </div>
@@ -216,7 +216,7 @@ function minorWarning() { ?>
                     case 'download': {
 
                             if ($version == 'v1') $zip_url = 'https://github.com/NamelessMC/Nameless/archive/v1.0.21.zip';
-                            else if ($version == 'v2') $zip_url = 'https://github.com/NamelessMC/Nameless/archive/v2.0.0-pr11.zip';
+                            else if ($version == 'v2') $zip_url = 'https://github.com/NamelessMC/Nameless/archive/v2.0.0-pr13.zip';
 
                             // Direct to selection screen if they went to an invalid version
                             else {
